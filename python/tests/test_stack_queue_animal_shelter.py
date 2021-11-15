3dad29c01afe556d69a7a3ee1658f9a1e67f4e49
hayaa1234 <hayalawansah@gmail.com>
2021-11-16 00:30:19 +0200

    trial 2

---
+++ python/tests/test_stack_queue_animal_shelter.py
@@ -0,0 +1,69 @@
+from stack_queue_animal_shelter.stack_queue_animal_shelter import Shelter,Cat,Dog
+import pytest
+
+
+def test_enqueue_into_queue(queue):
+    actual = str(queue.cats.front)
+    expected = str(Cat("lolo"))
+    assert expected ==actual
+
+# # Can successfully enqueue multiple values into a queue
+
+# def test_enqueue_multiple_values (queue):
+#     actual = str(queue)
+#     expected = '[1]->[2]->[3]->Null'
+#     assert actual == expected
+
+# # # # Can successfully dequeue out of a queue the expected value
+
+# def test_dequeue(queue):
+#     queue.dequeue()
+#     actual = queue.stack1.top.value
+#     expected = 2
+#     assert actual == expected
+
+# # # Can successfully peek into a queue, seeing the expected value
+
+# def test_peek_queue(queue):
+#     actual = queue.peek()
+#     expected = 1
+#     assert expected ==actual
+
+# # # Can successfully empty a queue after multiple dequeues
+
+# def test_empting_queue(queue):
+#     queue.dequeue()
+#     queue.dequeue()
+#     queue.dequeue()
+#     expected = None
+#     actual = queue.stack1.top   
+#     assert actual == expected
+
+
+# # # Can successfully instantiate an empty queue
+
+# def test_instantiate_empty_queue():
+#     queue = PsudoQueue()
+#     expected = None
+#     actual = queue.stack1.top
+#     assert expected == actual
+
+# # # Calling dequeue or peek on empty queue raises exception
+
+# def test_dequeue_or_peek_empty_queue():
+#     queue = PsudoQueue()
+#     with pytest.raises(ValueError):
+#         queue.dequeue()
+#     with pytest.raises(ValueError):
+#         queue.peek()
+
+
+@pytest.fixture
+def queue():
+    queue = Shelter()
+    queue.enqueue(Cat("lolo"))
+    queue.enqueue(Dog("mushmush"))
+    queue.enqueue(Dog("fofo"))
+    return queue
+
+
