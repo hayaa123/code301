f8b5e8d5f84b7a9bb983eb78b942f1e9bb606752
hayaa1234 <hayalawansah@gmail.com>
2021-12-06 23:28:30 +0200

    finished code challenge 27

---
+++ python/merge_sort/README.md
@@ -0,0 +1,20 @@
+# Merge Sort
+
+for information about insertion sort you can see my blog
+
+[blog](BLOG.md)
+
+you can find the code in the link below 
+
+[code](merge_sort.py)
+
+
+if you are interested in testing you can find it in the link below
+
+[testing](../tests/test_merge_sort.py)
+
+
+api
+
+insrtion sort -> this function is ment to sort the array in insertion sort algorithm 
+    it takes arr as input and return sorted array
---
+++ python/merge_sort/merge_sort.py
@@ -0,0 +1,37 @@
+
+def merge_sort(arr):
+    n= len(arr)
+
+    if n > 1 :
+        mid = n//2  
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort(left)
+
+        merge_sort(right)
+
+        merge(left,right,arr)
+        return arr
+    else:
+        return arr
+def merge(left,right,arr):
+    i=0
+    j=0
+    k=0 
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i = i + 1
+        else:
+            arr[k] = right[j]
+            j = j + 1
+
+        k = k + 1
+
+    if i == len(left):
+        arr[k:] = right[j:]
+    else:
+        arr[k:] = left[i:]
+
+
+
